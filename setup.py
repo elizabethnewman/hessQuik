ada94cefa23e840a428c400e2736b859acfb9442
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-06 15:24:25 -0500

    init commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hessQuik',
+    version='0.0.1',
+    packages=['hessQuik'],
+    url='https://github.com/elizabethnewman/hessQuik',
+    license='MIT',
+    author='Elizabeth Newman',
+    author_email='elizabeth.newman@emory.edu',
+    description='AD-free gradient and Hessian computations '
+)
