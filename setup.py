368637cb6d81b40ad352cd01522645c6b0d0ee1f
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-07 18:06:17 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='hessQuik',
     version='0.0.1',
-    packages=['hessQuik'],
+    packages=find_packages(include=['hessQuik']),
     url='https://github.com/elizabethnewman/hessQuik',
     license='MIT',
     author='Elizabeth Newman',
