5f6daf3b64d8d54d3bc0ecf0b4b678e5b6102a50
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-07 16:56:54 -0500

    updated required packages

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author='Elizabeth Newman',
     author_email='elizabeth.newman@emory.edu',
     description='AD-free gradient and Hessian computations',
-    install_requires=['torch>=1.9.0', 'numpy>=1.20.0'],
-    extras_require={'interactive': ['matplotlib>=2.2.0']}
+    install_requires=['torch', 'numpy'],
+    extras_require={'interactive': ['matplotlib']}
 )
