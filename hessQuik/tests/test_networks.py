64566f725bdbe5448ce1bbb3abd8a90d691fad72
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-08 11:17:17 -0500

    new network tests

--- hessQuik/tests/test_networks.py
+++ hessQuik/tests/test_networks.py
@@ -3,8 +3,21 @@ import unittest
 import torch
 import hessQuik.activations as act
 import hessQuik.layers as lay
-from hessQuik.networks import NN
+from hessQuik.networks import NN, fullyConnectedNN, resnetNN, ICNN
 from hessQuik.tests.utils import DerivativeCheckTestsNetwork
+
+
+def run_test(f, x, dx, requires_grad=False):
+    x.requires_grad = requires_grad
+
+    derivativeTests = DerivativeCheckTestsNetwork()
+
+    # forward tests
+    derivativeTests.run_forward_gradient_test(f, x, dx)
+    derivativeTests.run_forward_hessian_test(f, x, dx)
+
+    derivativeTests.run_backward_gradient_test(f, x, dx)
+    derivativeTests.run_backward_hessian_test(f, x, dx)
 
 
 class TestNetwork(unittest.TestCase):
@@ -25,26 +38,13 @@ class TestNetwork(unittest.TestCase):
 
         return f, x
 
-    @staticmethod
-    def run_test(f, x, dx, requires_grad=False):
-        x.requires_grad = requires_grad
-
-        derivativeTests = DerivativeCheckTestsNetwork()
-
-        # forward tests
-        derivativeTests.run_forward_gradient_test(f, x, dx)
-        derivativeTests.run_forward_hessian_test(f, x, dx)
-
-        derivativeTests.run_backward_gradient_test(f, x, dx)
-        derivativeTests.run_backward_hessian_test(f, x, dx)
-
     def test_NN_scalar_output(self):
         # problem setup
         f, x = self.setup_network(1)
         dx = torch.randn_like(x)
 
         print(self, ': scalar output')
-        self.run_test(f, x, dx, requires_grad=False)
+        run_test(f, x, dx, requires_grad=False)
 
     def test_NN_vector_output(self):
         # problem setup
@@ -53,42 +53,67 @@ class TestNetwork(unittest.TestCase):
         dx = torch.randn_like(x)
 
         print(self, ': vector output')
-        self.run_test(f, x, dx, requires_grad=False)
+        run_test(f, x, dx, requires_grad=False)
+
+
+class TestFullyConnectedNN(unittest.TestCase):
+
+    def test_fullyConnectedNN(self):
+        nex = 11
+        d = 3
+        ms = [d, 2, 7, 5]
+        x = torch.randn(nex, d)
+
+        f = fullyConnectedNN(ms, act=act.antiTanhActivation())
+        dx = torch.randn_like(x)
+
+        print(self)
+        run_test(f, x, dx, requires_grad=False)
+
+
+class TestResnetNN(unittest.TestCase):
+
+    def test_resnetNN(self):
+        nex = 11
+        d = 3
+        x = torch.randn(nex, d)
+
+        f = resnetNN(d, 4, h=0.25, act=act.quadraticActivation())
+        dx = torch.randn_like(x)
+
+        print(self)
+        run_test(f, x, dx, requires_grad=False)
 
 
 class TestICNNNetwork(unittest.TestCase):
 
-    @staticmethod
-    def setup_network(m):
+    def test_ICNNNetworkLayers(self):
         nex = 11  # no. of examples
         d = 3  # no. of input features
         ms = [None, 5, 2, 7]  # no. of output features
-        x = torch.randn(nex, d)
 
         f = NN(lay.ICNNLayer(d, ms[0], ms[1], act=act.softplusActivation()),
                lay.ICNNLayer(d, ms[1], ms[2], act=act.softplusActivation()),
                lay.ICNNLayer(d, ms[2], ms[3], act=act.softplusActivation()))
 
-        return f, x
-
-    @staticmethod
-    def run_test(f, x, dx, requires_grad=False):
-        x.requires_grad = requires_grad
-
-        derivativeTests = DerivativeCheckTestsNetwork()
-
-        # forward tests
-        derivativeTests.run_forward_gradient_test(f, x, dx, verbose=False)
-        derivativeTests.run_forward_hessian_test(f, x, dx)
-        derivativeTests.run_backward_gradient_test(f, x, dx)
-        derivativeTests.run_backward_hessian_test(f, x, dx)
-
-    def test_ICNN(self):
-        f, x = self.setup_network(1)
+        x = torch.randn(nex, d)
         dx = torch.randn_like(x)
 
-        print(self, ': vector output')
-        self.run_test(f, x, dx, requires_grad=False)
+        print(self)
+        run_test(f, x, dx, requires_grad=False)
+
+    def test_ICNN(self):
+        nex = 11  # no. of examples
+        d = 3  # no. of input features
+        ms = [None, 5, 2, 7]  # no. of output features
+
+        f = ICNN(d, ms, act=act.softplusActivation())
+
+        x = torch.randn(nex, d)
+        dx = torch.randn_like(x)
+
+        print(self)
+        run_test(f, x, dx, requires_grad=False)
 
 
 if __name__ == '__main__':
