e7d2d552433abc0eeb48a5d3b7c0e66967dd5812
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-08 01:12:08 -0500

    new import routines

---
+++ hessQuik/layers/__init__.py
@@ -0,0 +1,5 @@
+
+from .activation_functions import antiTanhActivation, quadraticActivation, softplusActivation, identityActivation
+from .single_layer import singleLayer
+from .resnet_layer import resnetLayer
+from .icnn_layer import ICNNLayer
