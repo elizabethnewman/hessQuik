aaae52f01897b6cb3335b00b0e79032f1521fe12
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-08 00:31:55 -0500

    added network tests

--- hessQuik/tests/test_networks.py
+++ hessQuik/tests/test_networks.py
@@ -5,7 +5,7 @@ import hessQuik.layers.activation_functions as act
 from hessQuik.layers.single_layer import singleLayer
 from hessQuik.layers.resnet_layer import resnetLayer
 from hessQuik.layers.icnn_layer import ICNNLayer
-from hessQuik,networks.network_wrapper import NN
+from hessQuik.networks.network_wrapper import NN
 from hessQuik.tests.utils import DerivativeCheckTestsNetwork
 
 
@@ -18,12 +18,12 @@ class TestNetwork(unittest.TestCase):
         ms = [2, 7, 5]
         x = torch.randn(nex, d)
 
-        f = net.NN(lay.singleLayer(d, ms[0], act=act.softplusActivation()),
-                   lay.singleLayer(ms[0], ms[1], act=act.softplusActivation()),
-                   lay.resnetLayer(ms[1], h=0.25, act=act.antiTanhActivation()),
-                   lay.singleLayer(ms[1], ms[2], act=act.softplusActivation()),
-                   lay.resnetLayer(ms[2], h=0.5, act=act.quadraticActivation()),
-                   lay.singleLayer(ms[2], m, act=act.softplusActivation()))
+        f = NN(singleLayer(d, ms[0], act=act.softplusActivation()),
+               singleLayer(ms[0], ms[1], act=act.softplusActivation()),
+               resnetLayer(ms[1], h=0.25, act=act.antiTanhActivation()),
+               singleLayer(ms[1], ms[2], act=act.softplusActivation()),
+               resnetLayer(ms[2], h=0.5, act=act.quadraticActivation()),
+               singleLayer(ms[2], m, act=act.softplusActivation()))
 
         return f, x
 
@@ -55,6 +55,41 @@ class TestNetwork(unittest.TestCase):
         dx = torch.randn_like(x)
 
         print(type(f), ': vector output')
+        self.run_test(f, x, dx, requires_grad=False)
+
+
+class TestICNNNetwork(unittest.TestCase):
+
+    @staticmethod
+    def setup_network(m):
+        nex = 11  # no. of examples
+        d = 3  # no. of input features
+        ms = [None, 5, 2, 7]  # no. of output features
+        x = torch.randn(nex, d)
+
+        f = NN(ICNNLayer(d, ms[0], ms[1], act=act.softplusActivation()),
+               ICNNLayer(d, ms[1], ms[2], act=act.softplusActivation()),
+               ICNNLayer(d, ms[2], ms[3], act=act.softplusActivation()))
+
+        return f, x
+
+    @staticmethod
+    def run_test(f, x, dx, requires_grad=False):
+        x.requires_grad = requires_grad
+
+        derivativeTests = DerivativeCheckTestsNetwork()
+
+        # forward tests
+        derivativeTests.run_forward_gradient_test(f, x, dx, verbose=False)
+        derivativeTests.run_forward_hessian_test(f, x, dx)
+        derivativeTests.run_backward_gradient_test(f, x, dx)
+        derivativeTests.run_backward_hessian_test(f, x, dx)
+
+    def test_ICNN(self):
+        f, x = self.setup_network(1)
+        dx = torch.randn_like(x)
+
+        print(type(f), ': scalar output')
         self.run_test(f, x, dx, requires_grad=False)
 
 
