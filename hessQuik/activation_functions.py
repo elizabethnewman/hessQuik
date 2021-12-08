95e6f555460973399491d73c19c2a0c1f1e9d6c2
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-07 23:40:13 -0500

    no message

--- hessQuik/activation_functions.py
+++ hessQuik/activation_functions.py
@@ -1,5 +1,20 @@
 import torch
-from layer_types import activationFunction
+from torch import Tensor
+from typing import Union, Tuple
+
+
+class activationFunction(torch.nn.Module):
+
+    def __init__(self) -> None:
+        super(activationFunction, self).__init__()
+        self.ctx = None  # context variable
+
+    def forward(self, x: Tensor, do_gradient: bool = False, do_Hessian: bool = False, reverse_mode: bool = False) -> \
+            Tuple[Tensor, Union[Tensor, None], Union[Tensor, None]]:
+        raise NotImplementedError
+
+    def backward(self, do_Hessian: bool = False) -> Tuple[Tensor, Union[Tensor, None]]:
+        raise NotImplementedError
 
 
 class antiTanhActivation(activationFunction):
