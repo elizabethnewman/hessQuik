63707be452683a2d336324e78018a5ee672576a8
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-17 00:52:08 -0500

    updated script

--- hessQuik/examples/ex_timing_test_hessQuik.py
+++ hessQuik/examples/ex_timing_test_hessQuik.py
@@ -1,10 +1,10 @@
 import torch
 from setup_timing_test import timing_test
 
-in_feature_range = (2 ** torch.arange(1, 10)).tolist()
+in_feature_range = (2 ** torch.arange(1, 11)).tolist()
 out_feature_range = (2 ** torch.arange(0, 1)).tolist()
 nex_range = [10]
-width = 100
+width = 20
 depth = 4
 num_trials = 10
 seed = 1234
@@ -13,7 +13,7 @@ seed = 1234
 torch.manual_seed(seed)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-network_type = 'hessQuik'
+network_type = 'PytorchAD'
 print(network_type)
 
 results = timing_test(in_feature_range, out_feature_range, nex_range,
