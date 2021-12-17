c50c391d89b51612acc45ccc8a16c3b514976a2f
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-17 15:43:32 -0500

    cleaned up timing test

--- hessQuik/examples/run_timing_test.py
+++ hessQuik/examples/run_timing_test.py
@@ -1,6 +1,6 @@
 import torch
 import pickle
-from setup_timing_test import timing_test
+from hessQuik.utils import timing_test
 import argparse
 from datetime import datetime
 import os
@@ -8,23 +8,25 @@ import os
 # -------------------------------------------------------------------------------------------------------------------- #
 # create parser
 parser = argparse.ArgumentParser(description='hessQuik-timing')
-parser.add_argument('--num-input',      type=int,               default=1,              metavar='n',
+parser.add_argument('--num-input',          type=int,               default=1,              metavar='n',
                     help='number of input features by powers of 2; start from 2^0 (default: 1 input features (2^0,)')
-parser.add_argument('--num-output',     type=int,               default=1,              metavar='m',
+parser.add_argument('--num-output',         type=int,               default=1,              metavar='m',
                     help='number of input features by powers of 2; start from 2^0 (default: 1 input features (2^0,)')
-parser.add_argument('--num-examples',   type=int,               default=1,              metavar='e',
-                    help='number of input features by powers of 10; start from 10^1 (default: 1 input features (10^1,)')
-parser.add_argument('--num-trials',     type=int,               default=10,             metavar='N',
+parser.add_argument('--num-examples',       type=int,               default=10,              metavar='e',
+                    help='number of examples')
+parser.add_argument('--num-trials',         type=int,               default=10,             metavar='N',
                     help='number of trials (default: 10)')
-parser.add_argument('--seed',           type=int,               default=42,             metavar='s',
+parser.add_argument('--seed',               type=int,               default=42,             metavar='s',
                     help='random seed (default: 42)')
-parser.add_argument('--width',          type=int,               default=16,             metavar='w',
+parser.add_argument('--width',              type=int,               default=16,             metavar='w',
                     help='width of network (default: 16)')
-parser.add_argument('--depth',          type=int,               default=4,              metavar='d',
+parser.add_argument('--depth',              type=int,               default=4,              metavar='d',
                     help='depth of network (default: 4)')
-parser.add_argument('--network-type',   type=str,               default='hessQuik',     metavar='t',
-                    help='type of network from ("hessQuiK", "PytorchAD", "PytorchHessian") (default: "hessQuiK")')
-parser.add_argument('--save',           action='store_true',    default=False,
+parser.add_argument('--network-type',       type=str,               default='resnet',     metavar='t',
+                    help='type of network from ("resnet", "fully_connected", "icnn") (default: "resnet")')
+parser.add_argument('--network-wrapper',    type=str,               default='hessQuik',     metavar='t',
+                    help='network wrapper from ("hessQuiK", "PytorchAD", "PytorchHessian") (default: "hessQuiK")')
+parser.add_argument('--save',               action='store_true',    default=False,
                     help='save results')
 args = parser.parse_args()
 
@@ -32,29 +34,39 @@ args = parser.parse_args()
 # setup parameters
 in_feature_range = (2 ** torch.arange(0, args.num_input)).tolist()
 out_feature_range = (2 ** torch.arange(0, args.num_output)).tolist()
-nex_range = (10 ** torch.arange(0, args.num_examples)).tolist()
+nex = args.num_examples
 width = args.width
 depth = args.depth
 num_trials = args.num_trials
 seed = args.seed
+network_type = args.network_type
+network_wrapper = args.network_wrapper
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-network_type = args.network_type
 
+print(args)
 # -------------------------------------------------------------------------------------------------------------------- #
 # filename
 now = datetime.now()
-my_date = now.strftime("%m-%d-%Y--")
+my_date = now.strftime("%m-%d-%Y-")
 
-filename = network_type + '-' + device + '-w' + str(width) + '-d' + str(depth)
+filename = (network_wrapper + '-' + network_type + '-' + device
+            + '-w' + str(width) + '-d' + str(depth) + '-out' + str(len(out_feature_range)))
 print(my_date + filename)
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # main
 torch.manual_seed(seed)
-results = timing_test(in_feature_range, out_feature_range, nex_range,
+
+# warm up
+warm_up = timing_test(in_feature_range, out_feature_range, nex,
+                      num_trials=2, width=width, depth=depth,
+                      network_wrapper=network_wrapper, network_type=network_type, device=device, clear_memory=True)
+
+# main run
+results = timing_test(in_feature_range, out_feature_range, nex,
                       num_trials=num_trials, width=width, depth=depth,
-                      network_type=network_type, device=device, clear_memory=True)
+                      network_wrapper=network_wrapper,  network_type=network_type, device=device, clear_memory=True)
 
 # -------------------------------------------------------------------------------------------------------------------- #
 if args.save:
--- hessQuik/utils/timing.py
+++ hessQuik/utils/timing.py
@@ -6,7 +6,7 @@ import time
 import gc
 
 
-def device_and_gradient_mapping(f, network_wrapper='hessQuik', device='cpu'):
+def setup_device_and_gradient(f, network_wrapper='hessQuik', device='cpu'):
 
     # map to device
     f = f.to(device)
@@ -51,8 +51,8 @@ def setup_icnn(in_features, out_features, width=20, depth=4):
     return f
 
 
-def create_network(in_features, out_features, width, depth, network_type='resnet',
-                   network_wrapper='hessQuik', device='cpu'):
+def setup_network(in_features, out_features, width, depth, network_type='resnet',
+                  network_wrapper='hessQuik', device='cpu'):
     if network_type == 'resnet':
         f = setup_resnet(in_features, out_features, width, depth)
     elif network_type == 'fully_connected':
@@ -62,7 +62,7 @@ def create_network(in_features, out_features, width, depth, network_type='resnet
     else:
         raise ValueError('network_type must be "resnet", "fully_connected", or "icnn"')
 
-    f, x_requires_grad = device_and_gradient_mapping(f, network_wrapper=network_wrapper, device=device)
+    f, x_requires_grad = setup_device_and_gradient(f, network_wrapper=network_wrapper, device=device)
 
     return f, x_requires_grad
 
@@ -102,36 +102,35 @@ def timing_test_gpu(f, x, num_trials=10, clear_memory=True):
     return total_time[1:]
 
 
-def timing_test(in_feature_range, out_feature_range, nex_range, num_trials=10, width=20, depth=4,
-                             network_wrapper='hessQuik', network_type='resnet', device='cpu', clear_memory=True):
+def timing_test(in_feature_range, out_feature_range, nex=10, num_trials=10, width=20, depth=4,
+                network_wrapper='hessQuik', network_type='resnet', device='cpu', clear_memory=True):
 
     # initialize
-    timing_trials = torch.zeros(len(in_feature_range), len(out_feature_range), len(nex_range), num_trials)
-    timing_trials_mean = torch.zeros(len(in_feature_range), len(out_feature_range), len(nex_range))
+    timing_trials = torch.zeros(len(in_feature_range), len(out_feature_range), num_trials)
+    timing_trials_mean = torch.zeros(len(in_feature_range), len(out_feature_range))
     timing_trials_std = torch.zeros_like(timing_trials_mean)
 
     for idx_out, out_features in enumerate(out_feature_range):
         for idx_in, in_features in enumerate(in_feature_range):
-            for idx_nex, nex in enumerate(nex_range):
 
-                # setup network
-                f, x_requires_grad = create_network(in_features, out_features, width, depth,  network_type=network_type,
-                                                    network_wrapper=network_wrapper, device=device)
+            # setup network
+            f, x_requires_grad = setup_network(in_features, out_features, width, depth,  network_type=network_type,
+                                               network_wrapper=network_wrapper, device=device)
 
-                # setup data
-                x = torch.randn(nex, in_features, device=device)
-                x.requires_grad = x_requires_grad
+            # setup data
+            x = torch.randn(nex, in_features, device=device)
+            x.requires_grad = x_requires_grad
 
-                # main test
-                if device == 'cpu':
-                    total_time = timing_test_cpu(f, x, num_trials=num_trials, clear_memory=clear_memory)
-                else:
-                    total_time = timing_test_gpu(f, x, num_trials=num_trials, clear_memory=clear_memory)
+            # main test
+            if device == 'cpu':
+                total_time = timing_test_cpu(f, x, num_trials=num_trials, clear_memory=clear_memory)
+            else:
+                total_time = timing_test_gpu(f, x, num_trials=num_trials, clear_memory=clear_memory)
 
-                # store results
-                timing_trials[idx_in, idx_out, idx_nex] = total_time
-                timing_trials_mean[idx_in, idx_out, idx_nex] = torch.mean(total_time).item()
-                timing_trials_std[idx_in, idx_out, idx_nex] = torch.std(total_time).item()
+            # store results
+            timing_trials[idx_in, idx_out] = total_time
+            timing_trials_mean[idx_in, idx_out] = torch.mean(total_time).item()
+            timing_trials_std[idx_in, idx_out] = torch.std(total_time).item()
 
     # store setup and results
     results = {'timing_trials': timing_trials,
@@ -139,7 +138,7 @@ def timing_test(in_feature_range, out_feature_range, nex_range, num_trials=10, w
                'timing_trials_std': timing_trials_std,
                'in_feature_range': in_feature_range,
                'out_feature_range': out_feature_range,
-               'nex_range': nex_range,
+               'nex': nex,
                'num_trials': num_trials}
 
     return results
