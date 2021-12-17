ad5fdf1de447e996b2326e3c44803768980b0dad
elizabethnewman <elizabeth.newman@emory.edu>
2021-12-16 19:36:19 -0500

    added a finite difference test

--- hessQuik/utils/input_derivative_check.py
+++ hessQuik/utils/input_derivative_check.py
@@ -12,6 +12,7 @@ def input_derivative_check(f, x, do_Hessian=False, forward_mode=True, num_test=1
     # ---------------------------------------------------------------------------------------------------------------- #
     # directional derivatives
     dx = torch.randn_like(x)
+    dx = dx / torch.norm(x)
     curvx = None
     if isinstance(f, hessQuik.activations.hessQuikActivationFunction):
         dfdx = df0 * dx
@@ -77,11 +78,81 @@ def input_derivative_check(f, x, do_Hessian=False, forward_mode=True, num_test=1
     return grad_check, hess_check
 
 
+def input_derivative_check_finite_difference(f, x, do_Hessian=False, forward_mode=True,
+                                             eps=1e-4, atol=1e-5, rtol=1e-3, verbose=False):
+
+    # compute initial gradient
+    f0, df0, d2f0 = f(x, do_gradient=True, do_Hessian=do_Hessian, forward_mode=forward_mode)
+
+    d = x.shape[1]
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # test gradient
+    df0_approx = torch.zeros_like(df0)
+    for i in range(d):
+        # perturbation in standard directions
+        ei = torch.zeros(d, device=f0.device, dtype=f0.dtype)
+        ei[i] = eps
+
+        f_pos, *_ = f(x + ei.unsqueeze(0))
+        f_neg, *_ = f(x - ei.unsqueeze(0))
+
+        df0_approx[:, i] = (f_pos - f_neg) / (2 * eps)
+
+    err = torch.norm(df0 - df0_approx).item()
+    rel_err = err / torch.norm(df0).item()
+
+    grad_check = (err < atol and rel_err < rtol)
+    if verbose:
+        print('Gradient Finite Difference: Error = %0.4e, Relative Error = %0.4e' % (err, rel_err))
+
+        if grad_check:
+            print('Gradient PASSED!')
+        else:
+            print('Gradient FAILED.')
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # test Hessian
+    # https://v8doc.sas.com/sashtml/ormp/chap5/sect28.htm
+    hess_check = None
+    if do_Hessian:
+
+        d2f0_approx = torch.zeros_like(d2f0)
+        for i in range(d):
+            ei = torch.zeros(d, device=f0.device, dtype=f0.dtype)
+            ei[i] = eps
+
+            for j in range(d):
+                ej = torch.zeros(d, device=f0.device, dtype=f0.dtype)
+                ej[j] = eps
+
+                f1, *_ = f(x + (ei + ej).unsqueeze(0))
+                f2, *_ = f(x + (ei - ej).unsqueeze(0))
+                f3, *_ = f(x + (-ei + ej).unsqueeze(0))
+                f4, *_ = f(x - (ei + ej).unsqueeze(0))
+
+                d2f0_approx[:, i, j] = (f1 - f2 - f3 + f4) / (4 * (eps ** 2))
+
+        err = torch.norm(d2f0 - d2f0_approx).item()
+        rel_err = err / max(torch.norm(d2f0).item(), eps)
+
+        hess_check = (err < atol and rel_err < rtol)
+        if verbose:
+            print('Hessian Finite Difference: Error = %0.4e, Relative Error = %0.4e' % (err, rel_err))
+
+            if hess_check:
+                print('Hessian PASSED!')
+            else:
+                print('Hessian FAILED.')
+
+    return grad_check, hess_check
+
+
 if __name__ == '__main__':
     from hessQuik.networks import NN
     from hessQuik.layers import singleLayer
-    from hessQuik.activations import softplusActivation
-    torch.set_default_dtype(torch.float32)
+    import hessQuik.activations as act
+    torch.set_default_dtype(torch.float64)
 
     nex = 11  # no. of examples
     d = 4  # no. of input features
@@ -89,8 +160,10 @@ if __name__ == '__main__':
     x = torch.randn(nex, d)
     dx = torch.randn_like(x)
 
-    f = NN(singleLayer(d, 7, act=softplusActivation()),
-           singleLayer(7, 5, act=softplusActivation()), reverse_mode=False)
+    f = NN(singleLayer(d, 7, act=act.identityActivation()),
+           singleLayer(7, 5, act=act.identityActivation()))
 
     input_derivative_check(f, x, do_Hessian=True, verbose=True)
+
+    input_derivative_check_finite_difference(f, x, do_Hessian=True, verbose=True, forward_mode=False)
 
