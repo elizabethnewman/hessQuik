0119f0028418e37e9fa5047b6f420c459562643c
elizabethnewman <elizabeth.newman@emory.edu>
2022-06-29 12:12:10 -0400

    removed picnn layer

--- hessQuik/layers/__init__.py
+++ hessQuik/layers/__init__.py
@@ -2,6 +2,5 @@ from .hessQuik_layer import hessQuikLayer
 from .single_layer import singleLayer
 from .resnet_layer import resnetLayer
 from .icnn_layer import ICNNLayer
-from .picnn_layer import PICNNLayer
 from .quadratic_layer import quadraticLayer
 from .quadratic_icnn_layer import quadraticICNNLayer
